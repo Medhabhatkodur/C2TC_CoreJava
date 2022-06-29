2e286488c9890f38587d9287ad162a42f7052823
Dell <Dell@DESKTOP-4K21F9Q>
2022-06-29 19:51:15 +0530

    first commit

---
+++ datatypes/src/com/cg/datatypes/datas.java
@@ -0,0 +1,12 @@
+package com.cg.datatypes;
+
+public class datas {
+
+	public static void main(String[] args) {
+	float f=20.5f;
+	int a=(int)f;
+	System.out.println(a);
+	System.out.println(f);
+	}
+
+}
