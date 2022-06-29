cf0a37540ac66cd3c160d430a99263cd507f8351
Dell <Dell@DESKTOP-4K21F9Q>
2022-06-29 19:50:48 +0530

    first commit

---
+++ operators1/src/com/cg/operators1/opera.java
@@ -0,0 +1,17 @@
+package com.cg.operators1;
+
+public class opera {
+	public static void main(String args[]) {
+int a=5;
+int b=18;
+if((a=3)==b)
+{
+System.out.println(++a);
+}
+else
+{
+	System.out.println(++b);
+}
+}
+}
+
