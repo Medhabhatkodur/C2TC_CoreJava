d7581afaf8dbbf8278604c122737c0d8b374ca70
Dell <Dell@DESKTOP-4K21F9Q>
2022-07-25 18:20:25 +0530

    first commit

---
+++ loopingStatment/src/com/cg/loopingStatment/ElseIf.java
@@ -0,0 +1,24 @@
+package com.cg.loopingStatment;
+
+public class ElseIf {
+
+	public static void main(String[] args) {
+		String lang="java";
+	if(lang=="python") 
+		{
+			System.out.println("lang is python");
+			
+		}
+		else if(lang=="c++") 
+		{
+			System.out.println("lang is c++");
+			
+		}
+		else if(lang=="java");
+		{
+		System.out.println("lang is java");
+		}
+		
+		
+		System.out.println("lang");}
+	}
