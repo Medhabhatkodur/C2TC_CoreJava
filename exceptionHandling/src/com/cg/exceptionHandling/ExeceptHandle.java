1e9c86dc04a52cfeeb2844b76cef673792eaddf5
Dell <Dell@DESKTOP-4K21F9Q>
2022-08-03 11:16:44 +0530

    first commit

---
+++ exceptionHandling/src/com/cg/exceptionHandling/ExeceptHandle.java
@@ -0,0 +1,29 @@
+package com.cg.exceptionHandling;
+
+public class ExeceptHandle {
+
+	public static void main(String[] args) {
+		try
+		{
+			
+			int a=0;
+			int b=10/a;
+			String name=null;
+			
+			System.out.println(name.length());
+		}
+		catch(ArithmeticException e) {
+			System.out.println("airthmetic exception");
+		}
+			catch(ArrayIndexOutOfBoundsException e) {
+				System.out.println("array index out of bound exeption");
+			}
+				catch(Exception e) {
+					System.out.println("parent exception");
+				}
+				System.out.println("rest of code");
+			
+		
+	}
+
+}
